657f5860224cc679bd2393e58df3c4df35687a06
Orr Sella <orr@sella.org>
2014-09-17 16:14:14 +0300

    Update elasticsearch, add commonsIO

--- memento/project/Dependencies.scala
+++ memento/project/Dependencies.scala
@@ -8,16 +8,17 @@ object Dependencies {
   // private val dispatch      = "net.databinder.dispatch" %% "dispatch-core"    % "0.11.0"
   private val json4s        = "org.json4s"              %% "json4s-native"    % "3.2.9"
   private val logback       = "ch.qos.logback"          %  "logback-classic"  % "1.1.1"
-  private val elasticsearch = "org.elasticsearch"       %  "elasticsearch"    % "1.2.1"
+  private val elasticsearch = "org.elasticsearch"       %  "elasticsearch"    % "1.3.2"
   private val jodatime      = "joda-time"               %  "joda-time"        % "2.3"
   private val jodaconvert   = "org.joda"                %  "joda-convert"     % "1.6"
   private val config        = "com.typesafe"            %  "config"           % "1.2.0"
   // private val commonsLang   = "org.apache.commons"      %  "commons-lang3"    % "3.3.1"
-  // private val commonsIo     = "commons-io"              %  "commons-io"       % "2.4"
+  private val commonsIo     = "commons-io"              %  "commons-io"       % "2.4"
   private val finatraDep    = "com.twitter"             %% "finatra"          % "1.5.3"
   private val finagleCore   = "com.twitter"             %% "finagle-core"     % "6.13.1"
   private val finagleHttp   = "com.twitter"             %% "finagle-http"     % "6.13.1"
   private val twitterUtil   = "com.twitter"             %% "util-core"        % "6.12.1"
+  private val elastic4s     = "com.sksamuel.elastic4s"  %% "elastic4s"        % "1.3.2"
 
   private val specs2Test  = specs2  % "test,it,e2e"
   private val mockitoTest = mockito % "test,it,e2e"
@@ -25,8 +26,8 @@ object Dependencies {
   val resolvers = Seq(Keys.resolvers += "twttr" at "http://maven.twttr.com/")
 
   val root            = deps()
-  val core            = deps(elasticsearch, jodatime, jodaconvert, config, specs2Test, mockitoTest, finagleCore, finagleHttp, twitterUtil, logback)
-  val coreTestkit     = deps(specs2, mockito, elasticsearch, json4s)
+  val core            = deps(elasticsearch, jodatime, jodaconvert, config, specs2Test, mockitoTest, finagleCore, finagleHttp, twitterUtil, logback, elastic4s)
+  val coreTestkit     = deps(specs2, mockito, elasticsearch, json4s, commonsIo)
   val finatra         = deps(finatraDep, specs2Test, config, json4s, logback)
   val finatraTestkit  = deps(finatraDep, specs2, mockito, json4s)
 
