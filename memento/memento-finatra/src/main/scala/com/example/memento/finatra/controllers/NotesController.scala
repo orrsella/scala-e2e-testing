d2a52beb61a6f833239e1e459e9a41b560d9348b
Orr Sella <orr@sella.org>
2014-09-20 23:54:55 +0300

    Remove NoteId (change to UUID)

--- memento/memento-finatra/src/main/scala/com/example/memento/finatra/controllers/NotesController.scala
+++ memento/memento-finatra/src/main/scala/com/example/memento/finatra/controllers/NotesController.scala
@@ -2,6 +2,7 @@ package com.example.memento.finatra.controllers
 
 import com.example.memento.core.concurrent.Implicits._
 import com.example.memento.core.dal.NotesDao
+import com.example.memento.core.exceptions.InvalidNoteIdException
 import com.example.memento.core.model.NewNote
 import com.example.memento.core.service.NotesService
 import com.example.memento.core.translation.Translator
@@ -11,6 +12,7 @@ import com.example.memento.finatra.responses.{AddNoteResponse, GetNoteResponse}
 import com.twitter.finatra.Request
 import java.util.UUID
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Try
 
 class NotesController(notesDao: NotesDao, translator: Translator) extends BaseController {
 
@@ -26,7 +28,8 @@ class NotesController(notesDao: NotesDao, translator: Translator) extends BaseCo
   }
 
   get("/notes/:id") { req: Request =>
-    val id = UUID.fromString(req.getRouteParam("id"))
+    val idParam = req.getRouteParam("id")
+    val id = Try(UUID.fromString(idParam)).getOrElse(throw new InvalidNoteIdException(idParam, null))
 
     service.getNote(id) map { opt =>
       val note = opt.getOrElse(throw new NoteNotFoundException(id))
@@ -35,8 +38,9 @@ class NotesController(notesDao: NotesDao, translator: Translator) extends BaseCo
   }
 
   get("/translate") { req =>
-    val id = UUID.fromString(req.getQueryParam("noteId"))
     val lang = req.getQueryParam("lang")
+    val idParam = req.getQueryParam("noteId")
+    val id = Try(UUID.fromString(idParam)).getOrElse(throw new InvalidNoteIdException(idParam, null))
 
     service.translateNote(id, lang) map { opt =>
       val note = opt.getOrElse(throw new NoteNotFoundException(id))
