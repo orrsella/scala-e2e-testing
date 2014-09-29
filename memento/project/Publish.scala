e4846e2685e6a76253fdffed4ccdf1d90b5d99b4
Orr Sella <orr@sella.org>
2014-09-29 12:22:00 +0300

    Add ability to publish/resolve artifacts from ivy repository on AWS S3

--- memento/project/Publish.scala
+++ memento/project/Publish.scala
@@ -1,5 +1,9 @@
 import sbt._
 import sbt.Keys._
+import ohnosequences.sbt.SbtS3Resolver._
+import ohnosequences.sbt.SbtS3Resolver.S3Resolver._
+import scala.language.postfixOps
+import com.amazonaws.services.s3.model.Region
 
 object Publish {
 
@@ -8,8 +12,14 @@ object Publish {
     publishLocal  := ()
   )
 
-  lazy val settings = Seq(
+  lazy val settings = S3Resolver.defaults ++ Seq(
     crossPaths        :=  false,
-    publishMavenStyle :=  false
+    publishMavenStyle :=  false,
+
+    s3region := Region.US_Standard,
+    publishTo := {
+      if (isSnapshot.value) None
+      else Some(s3resolver.value("Repository", s3("TODO")) withIvyPatterns)
+    }
   )
 }
--- memento/project/plugins.sbt
+++ memento/project/plugins.sbt
@@ -1,1 +1,5 @@
+resolvers += "Era7 maven releases" at "http://releases.era7.com.s3.amazonaws.com"
+
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.7.0-SNAPSHOT")
+
+addSbtPlugin("ohnosequences" % "sbt-s3-resolver" % "0.12.0")
